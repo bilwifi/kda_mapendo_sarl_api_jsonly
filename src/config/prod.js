3838b759d6b890d7c63c27608c361f3310eb504c
Christian Lisangola <christian.lisangola@gmail.com>
2020-03-18 23:36:38 +0100

    Renames Mongo remote db in config

--- src/config/prod.js
+++ src/config/prod.js
@@ -2,5 +2,5 @@ module.exports = {
   MONGO_USER: process.env.MONGO_USER_mapendo,
   MONGO_PASSWORD: process.env.MONGO_PASSWORD_mapendo,
   MONGO_DBNAME: process.env.MONGO_DBNAME_mapendo,
-  MONGO_URL: process.env.MONGO_URL_mapendo
+  MONGO_HOSTNAME: process.env.MONGO_URL_mapendo
 };
--- src/services/DBinit.js
+++ src/services/DBinit.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const CONFIG = require('../config/keys');
 let MONGO_URL;
 if (process.env.NODE_ENV === 'production') {
-  const { MONGO_DBNAME, MONGO_PASSWORD, MONGO_USER, MONGO_URL } = CONFIG;
-  MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_URL}/${MONGO_DBNAME}`;
+  const { MONGO_DBNAME, MONGO_PASSWORD, MONGO_USER, MONGO_HOSTNAME } = CONFIG;
+  MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DBNAME}`;
 } else {
   const { MONGO_DBNAME } = CONFIG;
   MONGO_URL = `mongodb://localhost/${MONGO_DBNAME}`;
